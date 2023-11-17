f9c930e1a18da23dec656f38f577e4fdcdbd6c9c
MINDQ <MINDQ@MINDQ_001>
2023-11-17 09:36:40 +0530

    String Examples

---
+++ Project_216/src/string/StringExamples.java
@@ -0,0 +1,12 @@
+package string;
+
+public class StringExamples {
+
+	public static void main(String[] args) {
+		
+		String name="WebDriver";
+		System.out.println(name.contains("Web"));
+
+	}
+
+}
